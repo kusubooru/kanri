60e5c19771c41df0aee14f5baa053d18627f2050
Kusubooru <kusubooru@gmail.com>
2016-05-02 16:35:45 +0300

    Add handler for submission of image ratings

--- main.go
+++ main.go
@@ -72,6 +72,7 @@ func main() {
 
 	http.Handle("/kanri", shim.Auth(ctx, app.serveIndex, *loginURL))
 	http.Handle("/kanri/safe", shim.Auth(ctx, app.serveSafe, *loginURL))
+	http.Handle("/kanri/safe/rate", shim.Auth(ctx, app.handleSafeRate, *loginURL))
 	http.Handle("/kanri/_image/", shim.Auth(ctx, app.serveImage, *loginURL))
 	http.Handle("/kanri/_thumb/", shim.Auth(ctx, app.serveThumb, *loginURL))
 	http.Handle("/kanri/login", newHandler(ctx, app.serveLogin))
@@ -118,6 +119,48 @@ func (app *App) serveSafe(ctx context.Context, w http.ResponseWriter, r *http.Re
 		return
 	}
 	app.render(w, safeTmpl, images)
+}
+
+func (app *App) handleSafeRate(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	// Only accept POST method.
+	if r.Method != "POST" {
+		http.Error(w, fmt.Sprintf("%v method not allowed", r.Method), http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Get user and user IP from context.
+	user, ok := ctx.Value("user").(*shimmie.User)
+	if !ok || user.Admin != "Y" {
+		http.Error(w, "You are not authorized to view this page.", http.StatusUnauthorized)
+		return
+	}
+	userIP := shimmie.GetOriginalIP(r)
+
+	// Get image ID and rating from the HTTP request.
+	id := r.PostFormValue("id")
+	imgID, err := strconv.Atoi(id)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("invalid image ID: %v", err), http.StatusBadRequest)
+		return
+	}
+	imgRating := r.PostFormValue("rating")
+
+	// Store the new image rating.
+	err = app.Shimmie.RateImage(imgID, imgRating)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Write the rating change to the shimmie log.
+	err = app.Shimmie.LogRating(imgID, imgRating, user.Name, userIP)
+	if err != nil {
+		log.Printf("Failed to log rating %q of image %d for username %q (%s) in score_log.",
+			imgRating, imgID, user.Name, userIP)
+	}
+
+	// Serve again the safe approval template.
+	app.serveSafe(ctx, w, r)
 }
 
 func (app *App) serveImage(ctx context.Context, w http.ResponseWriter, r *http.Request) {
